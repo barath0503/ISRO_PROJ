a43933ab9ba372495f9f1d1abbd13adfc193c6de
senettron-alpha <sibanaashi@gmail.com>
2026-05-03 22:53:27 +0530

    Delete rf_sync_dashboard directory

--- rf_sync_dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Distributed RF clock synchronization dashboard."""
-
-__all__ = ["create_app"]
-
-
-def create_app():
-    from rf_sync_dashboard.app import create_app as _create_app
-
-    return _create_app()
